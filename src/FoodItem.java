d70c1b808e80921b061e78e5c9765fd8dbf6e9ae
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-03-17 18:42:15 +0100

    add Fooditem class

---
+++ src/FoodItem.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class FoodItem {
+    String label;
+    double volume;
+    double weight;
+    Date expirationDate;
+
+    public FoodItem(String label) {
+        this.label = label;
+    }
+
+    public FoodItem() {
+    }
+
+}
